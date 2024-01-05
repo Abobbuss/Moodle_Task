47d0748ffec704bb0ce47e991578da9ec8eab022
Виктор <Vparipko007@gmail.com>
2024-01-05 15:44:31 +0700

    1

--- Scripts/Signaling.cs
+++ Scripts/Signaling.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Signaling : MonoBehaviour
@@ -7,16 +8,21 @@ public class Signaling : MonoBehaviour
 
     private const string _playerTag = "Player";
     private bool _isPlayerInside = false;
+    private bool _isAudioPlaying = false;
 
     private void Update()
     {
-        ChangeVolumeAudio(_isPlayerInside);
+        if (_isAudioPlaying)
+            StartCoroutine(ChangeVolumeOverTime(_isPlayerInside));
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag(_playerTag))
+        if (other.CompareTag(_playerTag))
+        {
             _isPlayerInside = true;
+            StartAudio();
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -25,11 +31,37 @@ public class Signaling : MonoBehaviour
             _isPlayerInside = false;
     }
 
-    private void ChangeVolumeAudio(bool isPlayerInside)
+    private IEnumerator ChangeVolumeOverTime(bool isPlayerInside)
     {
-        if (isPlayerInside)
-            _audioSource.volume += _fadeSpeed * Time.deltaTime;
-        else
-            _audioSource.volume -= _fadeSpeed * Time.deltaTime;
+        float targetVolume = isPlayerInside ? 1.0f : 0.0f;
+
+        while (!Mathf.Approximately(_audioSource.volume, targetVolume))
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, targetVolume, _fadeSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        if (_audioSource.volume == 0.0f)
+        {
+            StopAudio();
+        }
+    }
+
+    private void StartAudio()
+    {
+        if (_isAudioPlaying == false)
+        {
+            _isAudioPlaying = true;
+            _audioSource.Play();
+        }
+    }
+
+    private void StopAudio()
+    {
+        if (_isAudioPlaying)
+        {
+            _isAudioPlaying = false;
+            _audioSource.Stop();
+        }
     }
 }
